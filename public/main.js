fafcd1a289f95f46b05a3e0cdd96f770d7371870
Dusty <wajtejprotl@outlook.com>
2020-07-25 19:47:38 +0800

    静态服务器

---
+++ public/main.js
@@ -0,0 +1,36 @@
+ajax = (method, url, options) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open(method, url);
+        request.onreadystatechange = () => {
+            if (request.readyState === 4)
+                if (request.status < 400) {
+                    resolve.call(null, request.response)
+                } else {
+                    reject.call(null, request)
+                }
+        }
+        request.send()
+    })
+}
+
+
+ajax('GET', '/friends.json')
+    .then(
+        (response) => {
+            console.log('success')
+            friends = JSON.parse(response)
+            friends.forEach(item => {
+                $(".friends").append(`
+                    <li>
+                        <div>${item.name}</div>
+                        <div>${item.age}</div>
+                    </li>
+                `)
+            })
+        },
+        (response) => {
+            console.log('fail')
+            console.log(response.status);
+        }
+    )
