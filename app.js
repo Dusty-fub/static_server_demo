093753b4d477025a1239cd12891f10a575a5b5c3
Dusty <wajtejprotl@outlook.com>
2020-12-02 16:30:28 +0800

    初步实现app.use

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require('./express');
+const express = require("./express");
 
 let app = express();
 
-app.get('/',(req,res)=>{
-	res.end('hello world')
-})
+app.use("/", (req, res, next) => {
+  console.log("执行中间件");
+  res.setHeader("Content-Type", "text/html;charset=utf-8");
+  next();
+});
 
-app.post('/',(req,res)=>{
-	res.end('post name')
-})
+app.get("/", (req, res) => {
+  res.end("服务");
+});
 
-app.all('*',(req,res)=>{
-	res.end('all')
-})
+app.post("/", (req, res) => {
+  res.end("post name");
+});
 
-app.listen(3000,()=>{
-	console.log('server start at http://localhost:3000')
-})
+app.all("*", (req, res) => {
+  res.end("all");
+});
+
+app.listen(3000, () => {
+  console.log("server start at http://localhost:3000");
+});
--- express/index.js
+++ express/index.js
@@ -1,53 +1,84 @@
-const http = require('http')
-const url = require('url')
+const http = require("http");
+const url = require("url");
 
-function createApp(){
+function createApp() {
+  let app = (req, res) => {
+    let reqMethod = req.method.toLowerCase();
+    let { pathname } = url.parse(req.url, true);
 
-	let app = (req,res)=>{
+    let index = 0;
+    function next() {
+      if (index === app.routes.length) {
+        return res.end(`cannot ${reqMethod} ${pathname}`);
+      }
+      let { method, path, handler } = app.routes[index++];
+      if (method === "middle") {
+        if (
+          path === "/" ||
+          path === pathname ||
+          pathname.startsWith(path + "/")
+        ) {
+          handler(req, res, next);
+        } else {
+          next();
+        }
+      } else {
+        if (
+          (method === reqMethod || method === "all") &&
+          (path === pathname || path === "*")
+        ) {
+          handler(req, res);
+        } else {
+          next();
+        }
+      }
+    }
 
-		let reqMethod = req.method.toLowerCase();
-		let {pathname} = url.parse(req.url,true)
+    next();
+  };
 
-		app.routes.map((layer)=>{
-			let {method,path,handler} = layer;
-			if((method === reqMethod || method === 'all') && (path === pathname || path ==='*')){
-				handler(req,res);
-			}
-		})
+  app.routes = [];
 
-		res.end(`cannot ${reqMethod} ${pathname}`)
-	}
+  app.use = (path, handler) => {
+    if (typeof handler !== "function") {
+      handler = path;
+      path = "/";
+    }
+    let layer = {
+      method: "middle",
+      path,
+      handler,
+    };
+    app.routes.push(layer);
+  };
 
-	app.routes = []
+  app.all = (path, handler) => {
+    let layer = {
+      method: "all",
+      path,
+      handler,
+    };
+    app.routes.push(layer);
+  };
 
-	app.all = (path,handler)=>{
-		let layer = {
-				method:'all',
-				path,
-				handler
-			}
-		app.routes.push(layer)
-	}
+  http.METHODS.forEach((method) => {
+    method = method.toLowerCase();
+    app[method] = function (path, handler) {
+      let layer = {
+        method,
+        path,
+        handler,
+      };
+      app.routes.push(layer);
+    };
+  });
 
-	http.METHODS.forEach((method)=>{
-		method = method.toLowerCase();
-		app[method]= function(path,handler){
-			let layer = {
-				method,
-				path,
-				handler
-			}
-			app.routes.push(layer)
-		}
-	})
+  app.listen = function () {
+    let server = http.createServer(app);
+    server.listen(...arguments);
+  };
 
-	app.listen = function(){
-		let server = http.createServer(app)
-		server.listen(...arguments)
-	}
-
-	return app
+  return app;
 }
 
 module.exports = createApp;
-
